cf4f91ec15a439339eed98801c81228f0e75ae6a
s3725150 <s3725150@student.rmit.edu.au>
2020-05-17 01:06:51 +1000

    top 10 global stats

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ Initialization
 --------------------------------
 """
 # configuration
-DEBUG = True
+DEBUG = False
 
 # instantiate the app
 app = Flask(__name__)
@@ -62,10 +62,43 @@ def my_stats():
     stats = get_name_and_avatar(steamId)
     stats = merge(stats, get_total_playtime(steamId))
     stats = merge(stats, get_total_and_unplayed_games(steamId))
-    stats = merge(stats, get_top_played(steamId))
+    stats = merge(stats, get_top_played_games(steamId))
     return jsonify(stats)
 
+# Add friends and their games to DB then respond with stats
+@app.route('/friendStats', methods=['POST'])
+def friend_stats():
+    steamId = request.form.get("steamId")
+    friendIdList = db_add_friends_and_games(steamId)
+    statList = []
+    for sid in friendIdList:
+        stats = get_name_and_avatar(sid)
+        stats = merge(stats, get_total_playtime(sid))
+        stats = merge(stats, get_total_and_unplayed_games(sid))
+        statList.append(stats)
+    res = dict(friend_stats=statList)
+    return jsonify(res)
 
+# Global Stats
+@app.route('/globalStats', methods=['POST'])
+def global_stats():
+    steamId = request.form.get("steamId")
+    # myStats = get_name_and_avatar(steamId)
+    # myStats = merge(myStats, get_total_playtime_rank(steamId))
+    # myStats = merge(myStats, get_game_count_rank(steamId))
+    # myStats = merge(myStats, get_played_percent_rank(steamId))
+    # res = dict(user_stats=myStats)
+    # res = merge(res, get_global_top_playtime())
+    res = get_global_top_playtime()
+    res = merge(res, get_global_top_game_count())
+    res = merge(res, get_global_top_played_percent())
+    return jsonify(res)
+
+# Testing
+@app.route('/test', methods=['POST'])
+def test():
+    res = get_global_top_playtime()
+    return jsonify(res)
 
 """
 --------------------------------
@@ -119,24 +152,27 @@ def db_add_friends_and_games(steamId):
     friendList = friendList['friendslist']
     friendList['friends'] = [dict(steamid=key['steamid'])for key in friendList['friends']]
     length = len(friendList['friends'])
-    steamIdList = ""
+    commaSteamIdList = ""
+    steamIdList = []
     for i in range(length):
-        id = friendList['friends'][i]['steamid']
-        steamIdList += id
+        sid = friendList['friends'][i]['steamid']
+        steamIdList.append(sid)
+        commaSteamIdList += sid
         if i < length-1:
-            id += ","
-
-    summary = api_users_summary(steamIdList)
+            commaSteamIdList += ","
+    commaSteamIdList = commaSteamIdList.strip("76561198156863843,")
+    summary = api_users_summary(commaSteamIdList)
     summary = summary['response']
     summary['players'] = [dict(steamid=key['steamid'], personaname=key['personaname'], avatarmedium=key['avatarmedium']) for key in summary['players']]
 
     length = len(summary['players'])
     data = []
     for i in range(length):
-        id = summary['players'][i]['steamid']
+        sid = summary['players'][i]['steamid']
         name = summary['players'][i]['personaname']
         avatar_url = summary['players'][i]['avatarmedium']
-        data += [(id, name, avatar_url)]
+        tup = (sid, name, avatar_url)
+        data.append(tup)
 
     with database.batch() as batch:
         batch.insert_or_update(
@@ -146,29 +182,30 @@ def db_add_friends_and_games(steamId):
     print('Inserted / Updated Friends for ', steamId)
 
     for i in range(length):
-        id = summary['players'][i]['steamid']
-        db_add_games(id)
-    return
+        sid = summary['players'][i]['steamid']
+        db_add_games(sid)
+    return steamIdList
 
 def db_add_games(steamId):
     summary = api_get_owned_games(steamId)
     summary = summary['response']
-    summary['games'] = [dict(appid=key['appid'], name=key['name'], playtime_forever=key['playtime_forever']) for key in summary['games']]
+    if summary:
+        summary['games'] = [dict(appid=key['appid'], name=key['name'], playtime_forever=key['playtime_forever']) for key in summary['games']]
 
-    length = len(summary['games'])
-    data = []
-    for i in range(length):
-        appId = summary['games'][i]['appid']
-        name = summary['games'][i]['name']
-        playtime_forever = summary['games'][i]['playtime_forever']
-        data += [(appId, steamId, name, playtime_forever)]
+        length = len(summary['games'])
+        data = []
+        for i in range(length):
+            appId = summary['games'][i]['appid']
+            name = summary['games'][i]['name']
+            playtime_forever = summary['games'][i]['playtime_forever']
+            data += [(appId, steamId, name, playtime_forever)]
 
-    with database.batch() as batch:
-        batch.insert_or_update(
-            table='Games',
-            columns=('appId', 'steamId', 'name', 'playtime',),
-            values=data)
-    print('Inserted / Updated Games for ', steamId)
+        with database.batch() as batch:
+            batch.insert_or_update(
+                table='Games',
+                columns=('appId', 'steamId', 'name', 'playtime',),
+                values=data)
+        print('Inserted / Updated Games for ', steamId)
     return
 
 
@@ -190,7 +227,7 @@ def get_name_and_avatar(steamId):
             name = row[0]
             avatar = row[1]
 
-    res = dict(name=name, avatar=avatar)
+    res = dict(name=name, avatar_url=avatar)
     return res
 
 def get_total_playtime(steamId):
@@ -225,8 +262,7 @@ def get_total_and_unplayed_games(steamId):
     res = dict(total=total, unplayed=unplayed, unplayed_percent=int(unplayed_percent))
     return res
 
-def get_top_played(steamId):
-
+def get_top_played_games(steamId):
     with database.snapshot() as snapshot:
         query = "SELECT name, playtime FROM Games WHERE steamId = " + "'" + steamId + "' ORDER BY playtime DESC LIMIT 10"
         results = snapshot.execute_sql(query)
@@ -236,7 +272,101 @@ def get_top_played(steamId):
             playtime = row[1]/60
             game = dict(name=name, playtime=int(playtime))
             gameList.append(game)
-        res = dict(topPlayed=gameList)
+        res = dict(top_played=gameList)
+    return res
+
+
+def get_total_playtime_rank(steamId):
+    return
+def get_game_count_rank(steamId):
+    return
+def get_played_percent_rank(steamId):
+    return
+
+def get_global_top_playtime():
+    with database.snapshot() as snapshot:
+        query = ("SELECT t1.name1 AS id, t2.avatar, t1.playsum "
+                 "FROM ( "
+                 "SELECT g.steamId AS name1, SUM(g.playtime) AS playsum "
+                 "FROM Games g "
+                 "GROUP BY g.steamId "
+                 " ) t1 "
+                 "LEFT JOIN ( "
+                 "SELECT u.steamId AS name2, u.avatar_url AS avatar "
+                 "FROM Users u "
+                 "GROUP BY u.steamId, u.avatar_url "
+                 " ) t2 "
+                 "ON t2.name2 = t1.name1 "
+                 "ORDER BY t1.playsum DESC LIMIT 10 ")
+        results = snapshot.execute_sql(query)
+        resList = []
+        for row in results:
+            sid = row[0]
+            avatar = row[1]
+            playtime = row[2]/60
+            entry = dict(steamId=sid, avatar_url=avatar, playtime=int(playtime))
+            resList.append(entry)
+        res = dict(global_playtime=resList)
+    return res
+
+def get_global_top_game_count():
+    with database.snapshot() as snapshot:
+        query = ("SELECT t1.name1 AS id, t2.avatar, t1.gamecount "
+                 "FROM ( "
+                 "SELECT g.steamId AS name1, COUNT(g.appId) AS gamecount "
+                 "FROM Games g "
+                 "GROUP BY g.steamId "
+                 " ) t1 "
+                 "LEFT JOIN ( "
+                 "SELECT u.steamId AS name2, u.avatar_url AS avatar "
+                 "FROM Users u "
+                 "GROUP BY u.steamId, u.avatar_url "
+                 " ) t2 "
+                 "ON t2.name2 = t1.name1 "
+                 "ORDER BY t1.gamecount DESC LIMIT 10 ")
+        results = snapshot.execute_sql(query)
+        resList = []
+        for row in results:
+            sid = row[0]
+            avatar = row[1]
+            count = row[2]
+            entry = dict(steamId=sid, avatar_url=avatar, game_count=count)
+            resList.append(entry)
+        res = dict(global_game_count=resList)
+    return res
+
+def get_global_top_played_percent():
+    with database.snapshot() as snapshot:
+        query = ("SELECT t1.name1 AS id, t3.avatar, FLOOR((t1.played / t2.total)*100) AS percent "
+                 "FROM ( "
+                 "SELECT g.steamId AS name1, COUNT(g.appId) AS played "
+                 "FROM Games g "
+                 "WHERE playtime >=10 "
+                 "GROUP BY g.steamId "
+                 " ) t1 "
+                 "LEFT JOIN ( "
+                 "SELECT g.steamId AS name2, COUNT(g.appId) AS total "
+                 "FROM Games g "
+                 "GROUP BY g.steamId "
+                 " ) t2 "
+                 "ON t2.name2 = t1.name1 "
+                 "LEFT JOIN ( "
+                 "SELECT u.steamId AS name3, u.avatar_url AS avatar "
+                 "FROM Users u "
+                 "GROUP BY u.steamId, u.avatar_url "
+                 " ) t3 "
+                 "ON t3.name3 = t2.name2 "
+                 "ORDER BY percent DESC LIMIT 10")
+
+        results = snapshot.execute_sql(query)
+        resList = []
+        for row in results:
+            sid = row[0]
+            avatar = row[1]
+            percent = row[2]
+            entry = dict(steamId=sid, avatar_url=avatar, played_percent=percent)
+            resList.append(entry)
+        res = dict(global_play_percent=resList)
     return res
 
 
